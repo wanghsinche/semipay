b33d01294833dda82b1ee3972b4da636e7f10176
author <author@fourf>
2023-05-08 00:23:34 +0800

    fix: test crypto

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { createHmac } from 'node:crypto';
 
 type Data = {
-  name: string
+  name: string,
+  code: string,
+  saurce: string;
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const code = createHmac('sha256', 'mykey').update('101').digest('base64');
+  
+  res.status(200).json({ name: 'John Doe', code,  saurce: '101'})
 }
