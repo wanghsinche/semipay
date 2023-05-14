ca2d430c4f9e49118084b5fab60afe115ce1ba0b
author <author@fourf>
2023-05-15 01:09:33 +0800

    doc: use legacy frontpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useEffect, useState } from 'react'
-import SEMIPay from './main'
+import {SemiPay} from './legacy'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-return <SEMIPay />
+return <SemiPay />
 }
