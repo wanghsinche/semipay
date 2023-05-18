9afebd8516251e3871f47e21cc5e8c0d3cc5b2fd
wanghsinche <wanghsinche@hotmail.com>
2023-05-18 23:07:37 +0800

    email receipt (#3)
    * feat: send email
    
    * fix: email
    
    ---------

--- src/services/notifier.ts
+++ src/services/notifier.ts
@@ -120,3 +120,36 @@ async function sendTelegram(info: IInfo){
     `;
     await fetch(`${tg}&parse_mode=markdown&text=${encodeURIComponent(msg)}`)
 }
+
+export async function sendReceipt(info: IInfo ) {
+    
+    const hostname = await get('hostname') as string;
+    const suffix = new URL(hostname).hostname;
+
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let reply = await transporter.sendMail({
+    from: `"Fred Foo 👻" <foo@${suffix}>`, // sender address
+    to: info.user, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log(reply);
+
+}
