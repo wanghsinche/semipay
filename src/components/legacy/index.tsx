5bda4127e5eefab8a5da8504a3454e6a5effa447
author <author@fourf>
2023-05-29 11:06:14 +0800

    fix: title

--- src/components/legacy/index.tsx
+++ src/components/legacy/index.tsx
@@ -2,9 +2,13 @@ import { Inter } from 'next/font/google'
 import { Demo } from "./demo";
 import { Intro } from "./intro";
 import { Foot } from "./foot";
+import Head from 'next/head';
 
 export default function SemiPay() {
   return <main className="">
+    <Head>
+      <title>SEMIPAY</title>
+    </Head>
     <Intro />
     <Demo />
     <Foot />
